d1da041ab576eac402481a66d03a3dc21474d2e4
Azero 77 <zaggar.azero.m@gmail.com>
2024-08-12 13:54:04 +0300

    Added DataAccessClient and Models

---
+++ ConfigurationLibrary/DataAccessClient.cs
@@ -0,0 +1,69 @@
+﻿using ConfigurationLibrary.Exceptions;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationLibrary
+{
+    public class DataAccessClient
+    {
+        IConfigurationRoot Configuration { get; }
+        public DataAccessClient()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+        }
+
+        private OleDbConnection GetConnection() 
+        {
+            //Get the Connection String
+            string? connectionString = Configuration?["ConnectionString"];
+            //Initialize the connection
+            if (connectionString is not null)
+                return new OleDbConnection(connectionString);
+            else
+                throw new InvalidConnectionString();
+        }
+
+        #region Select
+        public async Task<IEnumerable<T>> Query<T>(string sql, object parameters)
+        {
+            IEnumerable<T>? Result;
+            try
+            {
+                OleDbConnection conn = GetConnection();
+                try
+                {
+                    //Open Connection
+                    await conn.OpenAsync();
+                    Result = await conn.QueryAsync<T>(sql, parameters);
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await conn.CloseAsync().ConfigureAwait(false);
+                }
+                return Result;
+            }
+            catch (InvalidConnectionString)
+            {
+
+                throw;
+            }
+            //run Query
+            //Close The Connection
+        }
+        #endregion
+    }
+}
---
+++ ShopApplication/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApplication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int? CategoryId { get; set; }
+        public int? ModelNumber { get; set; }
+        public string? ModelName { get; set; }
+        public decimal? Cost { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
